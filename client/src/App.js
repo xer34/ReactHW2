abc73fd266e85f6213c986d03c7a1043f232a671
Xer34 <experimental@gmail.com>
2019-01-25 20:16:55 -0500

    pages fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search"
 import Books from "./pages/Books";
-
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -12,9 +11,8 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Search} />
-          <Route exact path="/books" component={Books} />
-
+          <Route path="/" component={Search} />
+          <Route path="/books" component={Books} />
           <Route component={NoMatch} />
         </Switch>
       </div>
